6e75c9f8f8ad992aa40f4730c44cb2692ebe0d27
Angelo Leva <angelokta7@gmail.com>
2024-09-19 21:53:21 -0500

    chore: remove starlight (#7)

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,17 +3,9 @@ import { defineConfig } from "astro/config";
 
 import tailwind from "@astrojs/tailwind";
 
-import starlight from "@astrojs/starlight";
-
 // https://astro.build/config
 export default defineConfig({
   integrations: [
     tailwind(),
-    starlight({
-      title: "Wiki",
-      logo: {
-        src: "./src/assets/logo.svg",
-      },
-    }),
   ],
 });
