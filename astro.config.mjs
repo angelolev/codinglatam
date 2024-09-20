e8c480f947ba625dccbddd128b1a08c80ebc286a
Angelo Leva <angelokta7@gmail.com>
2024-09-19 21:19:16 -0500

    docs: add starlight pages (#4)

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,19 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
-import tailwind from '@astrojs/tailwind';
+import tailwind from "@astrojs/tailwind";
+
+import starlight from "@astrojs/starlight";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
+  integrations: [
+    tailwind(),
+    starlight({
+      title: "Wiki",
+      logo: {
+        src: "./src/assets/logo.svg",
+      },
+    }),
+  ],
 });
