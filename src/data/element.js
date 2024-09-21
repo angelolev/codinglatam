15cd9a17c8817a4f53d4233dbf229a9502a4b8be
aomerge <codenode.ortega@gmail.com>
2024-09-21 14:37:55 -0600

    feat: Add GridItem component and Cursos section
    - Added GridItem component to handle individual grid items in the Cursos section
    - Created Cursos section with a title, description, and a grid of courses
    - Imported data from element.js to populate the grid with course information
    - Implemented dynamic grid classes for each grid item based on its index
    - Added styling and layout adjustments to the GridItem component
    - Updated the GridItem component to conditionally render a button and its styling
    - Updated the Cursos section to display a message and description above the grid
    - Updated the Cursos section to use the GridItem component for each grid item
    - Added new SVG files for the course images

---
+++ src/data/element.js
@@ -0,0 +1,61 @@
+export const elementos = [
+    {
+      id: 1,
+      title: "DevOps",
+      description: "Aprende a implementar CI y CD para automatizar pruebas y despliegues.",
+      image: "./svg/Group.svg",      
+      hasButton: true,
+      reverseButton:false ,
+      buttonText: "Proximamente",
+      buttonClass: "mt-2 bg-primary-300 text-sm rounded-md text-[#171717] py-1 px-4",
+      flex: false
+    },
+    {
+      id: 2,
+      title: "DevOps",
+      description: "Aprende a implementar CI y CD para automatizar pruebas y despliegues.",
+      image: "./svg/bxl_react.svg",      
+      hasButton: true,
+      reverseButton:true ,
+      buttonText: "Enroll",
+      buttonClass: "mt-10 border border-primary-300 text-md rounded-md text-white py-1 px-4",
+      flex: true
+    },
+    {
+      id: 3,
+      title: "DevOps",
+      description: "Aprende a implementar CI y CD para automatizar pruebas y despliegues.",
+      image: "./svg/Group18.svg",
+      
+      hasButton: false,
+      flex: false
+    },
+    {
+      id: 4,
+      title: "DevOps",
+      description: "Aprende a implementar CI y CD para automatizar pruebas y despliegues.",
+      image: "./svg/Group16.svg",      
+      hasButton: true,
+      reverseButton:true ,
+      buttonText: "Enroll",
+      buttonClass: "mt-10 border border-primary-300 text-md rounded-md text-white py-1 px-4",
+      flex: true
+    },
+    {
+      id: 5,
+      title: "DevOps",
+      description: "Aprende a implementar CI y CD para automatizar pruebas y despliegues.",
+      image: "./svg/Group12.svg",      
+      hasButton: false,
+      flex: false
+    },
+    {
+      id: 6,
+      title: "DevOps",
+      description: "Aprende a implementar CI y CD para automatizar pruebas y despliegues.",
+      image: "./svg/Vector.svg",      
+      hasButton: false,
+      flex: false
+    },
+  ];
+  
