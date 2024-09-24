c567441e38068c484a1e5771e6485509faea6f81
Tyrone José <132106425+tyronejosee@users.noreply.github.com>
2024-09-24 18:41:19 -0300

    chore: add husky and commitlint configs (#10)

---
+++ commitlint.config.js
@@ -0,0 +1,6 @@
+export default {
+    extends: ['@commitlint/config-conventional'],
+    rules: {
+        'type-enum': [2, 'always', ['feat', 'fix', 'docs', 'style', 'refactor', 'test', 'chore']],
+    },
+};
