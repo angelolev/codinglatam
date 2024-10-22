585e1e223622f8941b743a159a35266dacf47174
Angelo Leva <angelokta7@gmail.com>
2024-10-21 19:32:49 -0500

    feat: add light and dark mode

--- src/islands/Nav.tsx
+++ src/islands/Nav.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState } from "react";
 import logoDark from "../assets/logo.svg";
 import burguer from "../assets/burguer.svg";
 
@@ -10,7 +10,7 @@ const Nav = () => {
   };
 
   return (
-    <nav className="flex justify-between items-center px-4 py-2 w-full bg-light-black relative max-w-6xl mx-auto">
+    <nav className="flex justify-between items-center px-4 py-2 w-full bg-white dark:bg-light-black fixed z-50 md:static max-w-6xl mx-auto">
       <div className="left">
         <a href="/" className="block">
           <img src={logoDark.src} alt="Logo" />
@@ -18,18 +18,43 @@ const Nav = () => {
       </div>
 
       <div className="right">
-        
-        <img src={burguer.src} onClick={toggleMenu} className="md:hidden" alt="Logo" />
+        <img
+          src={burguer.src}
+          onClick={toggleMenu}
+          className="md:hidden"
+          alt="Logo"
+        />
 
         <ul
           id="nav-links"
           className={`${
-            isOpen ? 'flex' : 'hidden'
-          } flex-col justify-center items-center bg-black-border py-6 px-4 gap-6 absolute top-12 left-0 w-full z-20 md:flex md:static md:flex-row md:bg-transparent md:w-auto md:gap-8`}
+            isOpen ? "flex" : "hidden"
+          } flex-col justify-center items-center bg-primary-300  py-6 px-4 gap-6 top-12 left-0 w-full  md:flex md:static md:flex-row md:bg-transparent md:w-auto md:gap-8 fixed min-h-dvh md:min-h-0`}
         >
-          <li><a href="/cursos" className="text-white hover:underline">Cursos</a></li>
-          <li><a href="/proyectos" className="text-white hover:underline">Proyectos</a></li>
-          <li><a href="/empleos" className="text-white hover:underline">Empleos</a></li>
+          <li>
+            <a
+              href="/cursos"
+              className="text-black/80 dark:text-white hover:underline"
+            >
+              Cursos
+            </a>
+          </li>
+          <li>
+            <a
+              href="/proyectos"
+              className="text-black/80 dark:text-white hover:underline"
+            >
+              Proyectos
+            </a>
+          </li>
+          <li>
+            <a
+              href="/empleos"
+              className="text-black/80 dark:text-white hover:underline"
+            >
+              Empleos
+            </a>
+          </li>
         </ul>
       </div>
     </nav>
